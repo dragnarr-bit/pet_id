04178179c1728d83a6bfd92044798c686dc0489f
dragnarr <99897386+dragnarr-bit@users.noreply.github.com>
2026-04-02 22:19:35 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,24 @@
+const pet = {
+  name: "Max",
+  age: "11 Months",
+  breed: "Belgian Malinois",
+  sex: "Male",
+  color: "Fawn/Tan with Black Mask",
+  owner: "Lhyam Khyle Paredes",
+  address: "Aquarius St., JR Torres Subd., Brgy. Singcang, Bacolod City",
+  phone: "09663784966 (Globe)",
+  image: "download.png"
+};
+
+document.getElementById("name").textContent = pet.name;
+document.getElementById("petNameBig").textContent = pet.name;
+document.getElementById("age").textContent = pet.age;
+document.getElementById("breed").textContent = pet.breed;
+document.getElementById("sex").textContent = pet.sex;
+document.getElementById("color").textContent = pet.color;
+
+document.getElementById("owner").innerHTML = "<strong>Owner:</strong> " + pet.owner;
+document.getElementById("address").innerHTML = "<strong>Address:</strong> " + pet.address;
+document.getElementById("phone").innerHTML = "<strong>Phone:</strong> " + pet.phone;
+
+document.getElementById("petImage").src = pet.image;
